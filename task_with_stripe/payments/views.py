74828408c5418f99c99d08c5e284987661f55597
Игорь Живов <izhivov97@yandex.ru>
2023-02-17 00:12:25 +0300

    реализовал ItemView для шаблона с выводом информации о товаре и реализовал api для получения id сессии

--- task_with_stripe/payments/views.py
+++ task_with_stripe/payments/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+import stripe
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Item
+
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+@api_view(['GET'])
+def buy(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    session = stripe.checkout.Session.create(
+        line_items=[{
+            'price_data': {
+                'currency': 'usd',
+                'product_data': {
+                    'name': item.name,
+                },
+                'unit_amount': item.price,
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url='http://localhost:4242/success',
+        cancel_url='http://localhost:4242/cancel',
+    )
+    return Response({'id': session.id})
+
+
+class ItemView(TemplateView):
+    template_name = 'buy_item.html'
+
+    def get_context_data(self, **kwargs):
+        item_pk = self.kwargs['pk']
+        item = get_object_or_404(Item, pk=item_pk)
+        context = super(ItemView, self).get_context_data(**kwargs)
+        context.update({
+            'item': item,
+            'STRIPE_PUBLIC_KEY': settings.STRIPE_PUBLIC_KEY
+        })
+        return context
