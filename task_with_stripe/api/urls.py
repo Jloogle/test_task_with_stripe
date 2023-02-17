adf114194e4802c5c5c3ab553c43ba7e5ca013f1
Игорь Живов <izhivov97@yandex.ru>
2023-02-17 17:07:15 +0300

    добавил api/urls

---
+++ task_with_stripe/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from api.views import ItemViewSet, buy
+
+
+router = routers.SimpleRouter()
+router.register(r'item', ItemViewSet, basename='item')
+
+app_name = 'api'
+
+urlpatterns = [
+    path('buy/<int:pk>/', buy, name='buy'),
+    path('', include(router.urls)),
+]
