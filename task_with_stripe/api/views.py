88d523f4de8959e1aa59c7dc78344fec39ee1ce1
Игорь Живов <izhivov97@yandex.ru>
2023-02-17 17:10:50 +0300

    перенес метод buy и добавил ViewSet для Item

---
+++ task_with_stripe/api/views.py
@@ -0,0 +1,38 @@
+import stripe
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import viewsets
+
+from payments.models import Item
+from .serializers import ItemSerializer
+
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+@api_view(['GET'])
+def buy(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    session = stripe.checkout.Session.create(
+        line_items=[{
+            'price_data': {
+                'currency': 'usd',
+                'product_data': {
+                    'name': item.name,
+                },
+                'unit_amount': item.price,
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url='http://localhost:4242/success',
+        cancel_url='http://localhost:4242/cancel',
+    )
+    return Response({'id': session.id})
+
+
+class ItemViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
