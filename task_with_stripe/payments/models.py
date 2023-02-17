c8a3b9c6e6d86ff2a4718fb63d9677f500baf388
Игорь Живов <izhivov97@yandex.ru>
2023-02-17 17:06:24 +0300

    добавил help_text

--- task_with_stripe/payments/models.py
+++ task_with_stripe/payments/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=155, verbose_name='Название')
     description = models.TextField('Описание')
-    price = models.IntegerField('Цена')
+    price = models.IntegerField('Цена', help_text='В центах')
 
     class Meta:
         verbose_name = 'Товар'
